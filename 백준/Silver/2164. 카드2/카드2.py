cd48207651bdc0559f0916d82df3bb0b64c5b8c7
CY <120891914+MiniMini-On@users.noreply.github.com>
2023-07-22 15:13:28 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 128136 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+
+
+f = sys.stdin.readline
+
+n = int(input())
+
+q= deque()
+for i in range(1, n+1):
+    q.append(i)
+    
+
+
+while len(q) >1:
+    q.popleft()
+    q.append(q.popleft())
+    
+print(q[0])
+    
